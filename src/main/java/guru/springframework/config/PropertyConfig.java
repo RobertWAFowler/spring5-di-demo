6100d57f995bdb45fa074490271565d0211b30a8
Robert Fowler <robertf@ipay.co.za>
2018-08-28 08:17:01 +0200

    Configure environment properties example.

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;    // Inject environment to get access to environment variables.
 
     @Value("${guru.username}")
     String user;
@@ -23,7 +28,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(user);
+//        fakeDataSource.setUsername(user);
+        fakeDataSource.setUsername(env.getProperty("G_USERNAME",user)); //Use environment variable and default if env property not set.
         fakeDataSource.setPassword(password);
         fakeDataSource.setDburl(url);
         return fakeDataSource;
